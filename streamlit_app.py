f7884f82f2cd70c244dafae4d50afe2956b326ab
Elijah <rennerelijah4@gmail.com>
2024-02-04 16:17:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,7 +126,7 @@ def generate_idea(HOBBY, PURPOSE, AUDIENCE, UNIQUENESS, HOURS):
     content_with_quotes = str(completion.choices[0].message.content)
     content_without_quotes = content_with_quotes[1:-1] if content_with_quotes.startswith('"') and content_with_quotes.endswith('"') else content_with_quotes
     st.write(content_without_quotes)
-    st.link_button("by Elijah Renner ", "https://streamlit.io/gallery")
+    st.link_button("by Elijah Renner", "https://streamlit.io/gallery")
 
 
 if __name__ == "__main__":
