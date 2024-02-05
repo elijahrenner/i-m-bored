6fcfff5729e40ff3290f1944358588ccfcd372d9
Elijah <rennerelijah4@gmail.com>
2024-02-05 17:49:32 -0500

    Really cool image

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,6 +77,8 @@ def main():
     st.title("I'm Bored 🥱")
     st.subheader("Bored? Let's find a project!")
     
+    st.image("Creative team-bro.svg")
+
     st.divider()
     HOBBY = ""
     selected_hobby = st.selectbox("Select from our list of hobbies.", ['', *hobbies])
